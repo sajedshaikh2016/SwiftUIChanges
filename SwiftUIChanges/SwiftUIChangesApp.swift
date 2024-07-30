b5d8b86922315a3693e9d6c1a4ebc423f7982a96
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-07-30 18:27:25 +0530

    Initial Commit

---
+++ SwiftUIChanges/SwiftUIChangesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChangesApp.swift
+//  SwiftUIChanges
+//
+//  Created by Sajed Shaikh on 30/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChangesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
